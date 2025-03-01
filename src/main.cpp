3e363099880bf33ffc78f35cadcbae76dac0039c
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-03-01 13:42:15 -0500

    put it inside the init

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
+namespace perms = geode::utils::permission;
 
 class $modify(MyMenuLayer, MenuLayer) {
 	bool init() {
 		if (!MenuLayer::init()) return false;
 		log::info("dankmeme01 custom test mod");
+		bool state = perms::getPermissionStatus(perms::Permission::RecordAudio);
+		log::debug("Initial state: {}", state);
+		perms::requestPermission(perms::Permission::RecordAudio, [](bool granted) {
+			log::debug("Was granted: {}", granted);
+		});
 		return true;
 	}
 };
-
-namespace perms = geode::utils::permission;
-bool state = perms::getPermissionStatus(perms::Permission::RecordAudio);
-log::debug("Initial state: {}", state);
-perms::requestPermission(perms::Permission::RecordAudio, [](bool granted) {
-    log::debug("Was granted: {}", granted);
-});
